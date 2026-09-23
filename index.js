be474c445985fc6865e00110bb734cb9d8d74c96
KMTonmoy <tonmoyahamed2009@gmail.com>
2026-09-23 20:49:13 +0600

    Final Update 1.0

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "http://localhost:3001",
+      "https://profitbooks.vercel.app",
       process.env.CLIENT_URL || "",
     ].filter(Boolean),
     credentials: true,
